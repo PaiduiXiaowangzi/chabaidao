21bf93812a57c2b078e2f7f2688b1d5d1f22ab7e
PDXWZ <3098448071@qq.com>
2024-08-30 19:36:53 +0800

    上传图片逻辑完成

--- app/router.js
+++ app/router.js
@@ -5,4 +5,5 @@ module.exports = app => {
   const { router, controller } = app;
   router.post('/api/admin/register',controller.admininfo.adminRegister)
   router.post('/api/admin/login',controller.admininfo.adminLogin)
+  router.post('/api/admin/uploadFile',controller.upload.uploadFile)
 };
--- config/plugin.js
+++ config/plugin.js
@@ -11,5 +11,9 @@ module.exports = {
   validate: {
     enable: true,
     package: 'egg-validate',
+  },
+  cors: {
+    enable: true,
+    package: 'egg-cors',
   }
 };
